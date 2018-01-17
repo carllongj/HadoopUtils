47e4b036f9c9f87577df7c34ad09ac2452395246
carl <carllongj@gmail.com>
2018-01-17 19:42:22 +0800

    多路径输出完成

--- src/cn/carl/hadoop/wrapper/MultipleOutputsWrapper.java
+++ src/cn/carl/hadoop/wrapper/MultipleOutputsWrapper.java
@@ -4,8 +4,8 @@ import cn.carl.hadoop.config.ContextConfig;
 import cn.carl.hadoop.mr.FileWrite;
 import cn.carl.hadoop.mr.WriteStrategy;
 import cn.carl.io.file.FileTools;
-import cn.carl.string.StringTools;
 import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,10 +24,9 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MultipleOutputsWrapper<K, V> extends MultipleOutputs<K, V> {
 
     /**
-     * 一个默认的输出数据路径
+     * 日志对象
      */
-    private static final String TEMP_PATH = System.getProperty("user.dir") +
-            File.separator + "temp_run";
+    private static final Logger LOGGER = Logger.getLogger(MultipleOutputsWrapper.class);
 
     /**
      * 设置当前的输出路径的根路径
@@ -70,10 +69,23 @@ public class MultipleOutputsWrapper<K, V> extends MultipleOutputs<K, V> {
         this.write(namedOutput, key, value, namedOutput);
     }
 
+    /**
+     * 指定一个路径输出
+     *
+     * @param namedOutput    指定名称的路径
+     * @param key            输出的KEY数据
+     * @param value          输出的VALUE数据
+     * @param baseOutputPath 指定的文件名称
+     * @param <K1>
+     * @param <V1>
+     * @throws IOException
+     * @throws InterruptedException
+     */
     @Override
     public <K1, V1> void write(String namedOutput, K1 key, V1 value, String baseOutputPath)
             throws IOException, InterruptedException {
-        String fileKey = baseOutputPath + namedOutput;
+
+        String fileKey = FileTools.toPathString(this.workspacePath, baseOutputPath);
 
         //获取当前的通道对象
         WriteStrategy writeStrategy = getWriteStrategy(fileKey, namedOutput, this.namedOutput);
@@ -82,16 +94,21 @@ public class MultipleOutputsWrapper<K, V> extends MultipleOutputs<K, V> {
         writeStrategy.write(key, value);
     }
 
+    /**
+     * 当前写出的数据方法,目前该方法不支持
+     *
+     * @param key            输出数据的KEY的数据
+     * @param value          输出数据的VALUE数据
+     * @param baseOutputPath 输出数据的路径
+     * @throws IOException
+     * @throws InterruptedException
+     */
     @Override
     public void write(K key, V value, String baseOutputPath)
             throws IOException, InterruptedException {
-
-
-        //获取当前路径的输出数据
-        WriteStrategy strategy = getWriteStrategy(baseOutputPath, null, baseNamedOutput);
-
-        //输出数据
-        strategy.write(key, value);
+        LOGGER.error("This method is not support");
+        throw new UnsupportedOperationException("This Class" + this.getClass().getName()
+                + " write(K,V,String) is not supported now");
     }
 
     /**
@@ -99,62 +116,40 @@ public class MultipleOutputsWrapper<K, V> extends MultipleOutputs<K, V> {
      *
      * @param baseOutputPath 数据路径的key数据
      * @param namedPath      输出的文件名称
-     * @param outputs        输出管道
-     * @return
+     * @param outputs        输出管道集合
+     * @return 写出数据的对象
      */
     private WriteStrategy getWriteStrategy(String baseOutputPath, String namedPath,
                                            Map<String, WriteStrategy> outputs) {
 
-        WriteStrategy fileWrite = null;
+        //声明一个写出策略方式
+        WriteStrategy writeStrategy;
 
-        if (null == outputs.get(baseOutputPath)) {
+        //获取当前的输出数据
+        String pathString = FileTools.toPathString(baseOutputPath, namedPath);
 
+        if (outputs.containsKey(pathString)) {
 
-            //获取当前的输出路径
-            String realPath = null;
-
-            if (StringTools.hasText(namedPath) && StringTools.hasText(baseOutputPath)) {
-
-                //当前有输出数据路径
-                realPath = baseOutputPath + namedPath;
-
-            } else if (!StringTools.hasText(baseOutputPath) || this.workspacePath.equals(baseOutputPath)) {
-                //声明当前的文件
-                File file;
-
-                //如果改路径为空或者是和指定输出路径冲突,则重新指定默认路径作为输出
-                //创建该文件对象
-                file = new File(baseOutputPath);
-
-                //如果当前文件存在或者文件是一个文件夹
-                if (file.exists() || file.isDirectory()) {
-
-                    //重新创建一个路径,使用默认的路径
-                    file = new File(TEMP_PATH);
-
-                    //如果还是存在
-                    if (file.exists() || file.isDirectory()) {
-
-                        //强制删除这个文件或者文件夹
-                        FileTools.forceDeleteFile(file);
-                        realPath = file.getPath();
-                    }
-                }
-            }
-
-            //创建改路径下的数据文件
-            fileWrite = new FileWrite(realPath);
-
-            //将数据输出放到该映射集合中
-            outputs.put(realPath, fileWrite);
+            //获取上一次的输出数据通道
+            writeStrategy = outputs.get(pathString);
 
         } else {
 
-            //否则获取该通道
-            outputs.get(baseOutputPath);
+            //创建一个文件对象
+            File file = new File(pathString);
+
+            //当前如果存在此路径且此路径是一个文件夹,删除该文件和目录
+            if (file.exists() && file.isDirectory()) {
+
+                //强制递归删除该文件或目录
+                FileTools.forceDeleteFile(file, true);
+            }
+
+            //获取当前的输出数据通道
+            writeStrategy = new FileWrite(pathString);
         }
 
-        return fileWrite;
+        return writeStrategy;
     }
 
     @Override
